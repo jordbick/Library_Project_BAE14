76f7c52b9c845052c8af1ed671f367c8af91e694
Jordan <jordanbickerdyke@hotmail.co.uk>
2022-04-25 12:19:15 +0100

    JB: init commit

---
+++ src/main/java/com/qa/library/LibraryProjectBae14Application.java
@@ -0,0 +1,13 @@
+package com.qa.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectBae14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProjectBae14Application.class, args);
+	}
+
+}
