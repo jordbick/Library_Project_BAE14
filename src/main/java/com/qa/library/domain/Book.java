41c8af5a93d1fd94ff85ba06649d47b0e3626fbc
Jordan <jordanbickerdyke@hotmail.co.uk>
2022-04-25 16:52:37 +0100

    JB: Added toString, hashCode and equals method to book entity

--- src/main/java/com/qa/library/domain/Book.java
+++ src/main/java/com/qa/library/domain/Book.java
@@ -1,5 +1,7 @@
 package com.qa.library.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -58,8 +60,87 @@ public class Book {
 		this.publisher = publisher;
 		this.rating = rating;
 	}
-	
-	
-	
 
+	// Getters and Setters ----------------------------------------------
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getPublishedYear() {
+		return publishedYear;
+	}
+
+	public void setPublishedYear(int publishedYear) {
+		this.publishedYear = publishedYear;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	
+	// toString -------------------------------------------------------------
+	
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publishedYear=" + publishedYear
+				+ ", publisher=" + publisher + ", rating=" + rating + "]";
+	}
+
+	// hashCode and equals ---------------------------------------------------
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(author, id, publishedYear, publisher, rating, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(author, other.author) && id == other.id && publishedYear == other.publishedYear
+				&& Objects.equals(publisher, other.publisher) && rating == other.rating
+				&& Objects.equals(title, other.title);
+	}
+	
+	
+	
 }
