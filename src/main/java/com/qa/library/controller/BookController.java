8c9a57d1029d1c587fea7b67dc345e4c1075e338
Jordan <jordanbickerdyke@hotmail.co.uk>
2022-04-26 09:11:31 +0100

    JB: Added GET methods to controller class

--- src/main/java/com/qa/library/controller/BookController.java
+++ src/main/java/com/qa/library/controller/BookController.java
@@ -1,10 +1,41 @@
 package com.qa.library.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.library.domain.Book;
+import com.qa.library.service.BookService;
 
 @RestController
 @CrossOrigin
+@RequestMapping(path = "/book")
 public class BookController {
+	
+	private BookService service;
+
+	@Autowired
+	public BookController(BookService service) {
+		super();
+		this.service = service;
+	}
+	
+	// GET -------------------------------------------------
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Book>> getAll(){
+		return new ResponseEntity<List<Book>>(service.getAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/getById/{id}")
+	public ResponseEntity<Book> getById(@PathVariable long id){
+		return new ResponseEntity<Book>(service.getById(id), HttpStatus.OK);
+	}
 
 }
