dea3807cf0722beaa970e626bec6bac432fe593a
Jordan <jordanbickerdyke@hotmail.co.uk>
2022-04-26 08:35:26 +0100

    JB: Added GET methods to service class

--- src/main/java/com/qa/library/service/BookService.java
+++ src/main/java/com/qa/library/service/BookService.java
@@ -1,5 +1,37 @@
 package com.qa.library.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.library.domain.Book;
+import com.qa.library.repo.BookRepo;
+
+@Service
 public class BookService {
+	
+	private BookRepo repo;
+
+	@Autowired
+	public BookService(BookRepo repo) {
+		super();
+		this.repo = repo;
+	}
+	
+	// GET ---------------------------------------------------------
+	
+	public List<Book> getAll(){
+		return repo.findAll();
+	}
+	
+	public Book getById(long id) {
+		return repo.findById(id).get();
+	}
+
+	// PUT -----------------------------------------------------------
+	
+	
 
 }
