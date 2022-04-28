cd9c78bd71bbd37c81bd53c128aeb639486bfedb
Jordan <jordanbickerdyke@hotmail.co.uk>
2022-04-28 10:00:52 +0100

    JB: Added PUT method to front-end

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,13 +13,20 @@ const PUBLISHER_EL = document.querySelector(".publisher-el");
 const PUBLISHED_YEAR_EL = document.querySelector(".published-year-el");
 const RATING_EL = document.querySelector(".rating-el");
 
-const radioButtons = document.querySelectorAll('input[name="rating"]');
+const TITLE_UPDATE = document.querySelector(".title-update");
+const AUTHOR_UPDATE = document.querySelector(".author-update");
+const PUBLISHER_UPDATE = document.querySelector(".publisher-update");
+const PUBLISHED_YEAR_UPDATE = document.querySelector(".published-year-update");
+const RATING_UPDATE = document.querySelector(".rating-update");
+
+const RADIO_BUTTONS = document.querySelectorAll('input[name="rating"]');
 let selectedRating;
 
 // Buttons
 const CREATE_BTN = document.querySelector("#create-btn");
+const EDIT_BTN = document.querySelector("#edit-btn");
 
-// Get request
+// GET ALL request --------------------------------------------------------------------------
 const getAll = () => {
   axios
     .get(`${ADDR}/book/getAll`)
@@ -33,9 +40,9 @@ const getAll = () => {
     .catch((err) => console.error(err));
 };
 
-// Post request
+// POST request -------------------------------------------------------------------------------
 function create() {
-  for (const radioButton of radioButtons) {
+  for (const radioButton of RADIO_BUTTONS) {
     if (radioButton.checked) {
       selectedRating = parseInt(radioButton.value);
     }
@@ -47,12 +54,34 @@ function create() {
     publisher: PUBLISHER_EL.value,
     rating: selectedRating,
   };
-  console.log(data);
   axios
     .post(`${ADDR}/book/create`, data)
     .then((res) => console.log(res))
     .catch((err) => alert(err));
   getAll;
+}
+
+// PUT request --------------------------------------------------------------------------
+function update() {
+  for (const radioButton of RADIO_BUTTONS) {
+    if (radioButton.checked) {
+      selectedRating = parseInt(radioButton.value);
+    }
+  }
+  const data = {
+    title: editForm.title.value,
+    author: editForm.author.value,
+    publishedYear: editForm.publishedYear.value,
+    publisher: editForm.publisher.value,
+    rating: selectedRating,
+  };
+
+  console.log(data);
+
+  axios
+    .put(`${ADDR}/book/update/${parseInt(editForm.entryId.value)}`, data)
+    .then((res) => console.log(res))
+    .catch((err) => alert(err));
 }
 
 const printResult = (result) => {
@@ -66,30 +95,60 @@ const printResult = (result) => {
   VALUES.setAttribute("class", "values");
   VALUES.textContent = `Title: ${result.title} | Author: ${result.author} | Year Published: ${result.publishedYear} | Publisher: ${result.publisher} | Rating : ${result.rating}/5`;
 
+  const BUTTONS = document.createElement("div");
+  BUTTONS.role = "group";
+  BUTTONS.class = "btn-group";
+
   const EDIT = document.createElement("button");
   EDIT.type = "button";
   EDIT.textContent = "Edit";
   EDIT.id = result.id;
-  EDIT.setAttribute("class", "btn edit");
+  EDIT.setAttribute("class", " btn btn-sm btn-warning edit-btn");
   EDIT.setAttribute("onClick", "openEdit(this.id)");
 
   const DEL = document.createElement("button");
   DEL.type = "button";
   DEL.textContent = "Delete";
-  DEL.id = `${result.id}`;
+  DEL.id = result.id;
   DEL.setAttribute("class", "btn btn-sm btn-danger del-btn");
   DEL.setAttribute("onClick", "del(this.id)");
+
+  const LINEBREAK = document.createElement("br");
 
   INNER_DIV.appendChild(VALUES);
   OUTER_DIV.appendChild(INNER_DIV);
 
-  VALUES.appendChild(EDIT);
-  VALUES.appendChild(DEL);
+  VALUES.appendChild(BUTTONS);
+  BUTTONS.appendChild(EDIT);
+  BUTTONS.appendChild(DEL);
+  VALUES.appendChild(LINEBREAK);
 
   RESULTS_DIV.appendChild(OUTER_DIV);
 };
 
+const openEdit = (id) => {
+  // Show modal and configure date field
+  $("#edit-modal").modal("show");
+
+  // Get the current values for selected entry
+  axios
+    .get(`${ADDR}/book/getById/${id}`)
+    .then((res) => {
+      const ENTRY = res.data;
+      // Populate modal form with current values
+      const EDIT_FORM = document.forms["editForm"];
+      EDIT_FORM["title"].value = ENTRY.title;
+      EDIT_FORM["author"].value = ENTRY.author;
+      EDIT_FORM["publishedYear"].value = ENTRY.publishedYear;
+      EDIT_FORM["publisher"].value = ENTRY.publisher;
+      EDIT_FORM["rating"].value = ENTRY.rating;
+      EDIT_FORM["entryId"].value = ENTRY.id;
+    })
+    .catch((err) => console.error(err));
+};
+
 // Event Listener
 CREATE_BTN.addEventListener("click", create);
+EDIT_BTN.addEventListener("click", update);
 
 getAll();
